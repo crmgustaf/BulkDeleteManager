8efc644ca4a1ba4fc4bae82dddd271df95e2ff27
Gustaf Westerlund <gustaf.westerlund@apollo.se>
2023-05-28 14:35:13 +0200

    Changed version info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gustaf Westerlund, Ebba Linnea Nilsson and Jonas Rapp")]
 [assembly: AssemblyProduct("BulkDeleteManager")]
-[assembly: AssemblyCopyright("Copyright © Gustaf Westerlund, Ebba Linnea Nilsson and Jonas Rapp 2022")]
+[assembly: AssemblyCopyright("Copyright © Gustaf Westerlund, Ebba Linnea Nilsson and Jonas Rapp 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
